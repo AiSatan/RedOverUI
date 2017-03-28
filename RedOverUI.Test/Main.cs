5ac93a44ceda08c6edadacb41b275434552db499
Fandus Atipov <aisatan.ne@live.com>
2017-03-29 00:48:27 +0300

    added test, fix repo

---
+++ RedOverUI.Test/Main.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Red.BitmapHelpers;
+
+namespace RedOverUI.Test
+{
+    [TestClass]
+    public class Main
+    {
+        [TestMethod]
+        public void SimpleRun()
+        {
+            var resetEvent = new AutoResetEvent(false);
+            RedOverlay.OnShown += () =>
+            {
+                RedOverlay.Refresh();
+                resetEvent.Set();
+            };
+
+            RedOverlay.OnUpdate += g =>
+            {
+                g.FillRectangle(Brushes.Red, new Rectangle(0, 0, 10, 10));
+            };
+
+            resetEvent.WaitOne(5000);
+
+            using (var bmpScreenCapture = new Bitmap(10, 10))
+            {
+                using (var g = Graphics.FromImage(bmpScreenCapture))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, bmpScreenCapture.Size, CopyPixelOperation.SourceCopy);
+                }
+                var pixels = bmpScreenCapture.BitmapToByteRgbQ();
+
+                for (var x = 0; x < bmpScreenCapture.Width; x++)
+                {
+                    for (var y = 0; y < bmpScreenCapture.Height; y++)
+                    {
+                        var color = pixels.GetColor(x, y);
+                        Assert.IsTrue(color.R == Color.Red.R);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RedOverUI.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RedOverUI.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RedOverUI.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50f3ac3c-c8c3-4a5f-ac43-0be350be4881")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
